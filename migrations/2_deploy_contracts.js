287c7ac773421f19ef294130ca012e4293a6f82c
Luca Marangoni <luk.ma88@gmail.com>
2022-06-07 14:53:31 +0200

    minor fixes on migrations file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const treasury = artifacts.require("TreasuryWithAllowance")
-const conflict = artifacts.require("I3MarketConflicts")
+//const conflict = artifacts.require("I3MarketConflicts")
 
 module.exports = function (deployer) {
     deployer.deploy(treasury);
-    deployer.deploy(conflict)
+//    deployer.deploy(conflict)
 }
